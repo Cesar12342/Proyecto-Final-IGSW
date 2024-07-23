9c82eeb9c415f4ba64a8216d58038d9988623981
Cesar12342 <perezcesaralejandro0@gmail.com>
2024-07-23 14:32:25 -0400

    eliminar las clases (ActualizarCancelarReserva y ConfirmarReserva) y agregar la clase FuncionesReservas

---
+++ Sistema de reservas para un restaurante/src/sistemaDeReservasParaUnRestaurante/FuncionesReservas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sistemaDeReservasParaUnRestaurante;
+import java.util.ArrayList;
+/**
+ *
+ * @author César
+ */
+public class FuncionesReservas {
+    private ArrayList<ReservaMesa>reservas=new ArrayList<>(); 
+
+    public ArrayList<ReservaMesa> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(ArrayList<ReservaMesa> reservas) {
+        this.reservas = reservas;
+    }
+ 
+   }
+
+    
+
--- Sistema de reservas para un restaurante/src/sistemaDeReservasParaUnRestaurante/SistemaDeReservasParaUnRestaurante.java
+++ Sistema de reservas para un restaurante/src/sistemaDeReservasParaUnRestaurante/SistemaDeReservasParaUnRestaurante.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package sistema.de.reservas.para.un.restaurante;
+package sistemaDeReservasParaUnRestaurante;
 
 /**
  *
